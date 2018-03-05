ede78fb44a4eb1f3570f5f4632a403517ec807e1
Russell Hyde <russ_hyde@hotmail.com>
2018-03-05 15:59:55 +0000

    skeleton files

--- R/coxpresdbr_parse.R
+++ R/coxpresdbr_parse.R
@@ -2,3 +2,13 @@
 # Parsing functions for `coxpresdbr` package
 #
 ###############################################################################
+
+get_coex_partners <- function(
+                              gene_ids,
+                              db_archive,
+                              gene_universe = NULL,
+                              n_partners = 100,
+                              cor_threshold = -1,
+                              mr_threshold = 0) {
+  NULL
+  }
