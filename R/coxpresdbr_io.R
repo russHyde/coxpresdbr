3a1d3358a6f66b0bb46a8dc028e5956aad028ffb
Russell Hyde <russ_hyde@hotmail.com>
2018-03-05 15:14:19 +0000

    adds functions to import the dataset for a single gene from a coxpresdb archive
    runs without unzipping / detarring the archive
    
    tests for the functions are included

--- R/coxpresdbr_io.R
+++ R/coxpresdbr_io.R
@@ -3,15 +3,115 @@
 #
 ###############################################################################
 
-# data.table::fread("tar --to-stdout -xjf {archive_name} {file_name}")
-#
-#' @export
+#' Checks whether a provided file-path points to a valid CoxpresDB.jp archive
 #'
-import_coex_db <- function(gene_id, db_archive) {
-  stopifnot(file.exists(db_archive))
+#' @param        db_archive    A file path. Should be a single \code{*.tar.bz2}
+#' file as downloaded from the coxpresdb.jp website.
+#'
+#' @importFrom   stringr       str_detect
+#'
+.is_coxpresdb_archive <- function(
+                                  db_archive) {
+  length(db_archive) == 1 &&
+    file.exists(db_archive) &&
+    stringr::str_detect(db_archive, "tar.bz2$")
 }
 
-# system("tar --list -jf {archive_name} | grep -v '/$'", intern = TRUE)
-#
-get_coex_db_universe <- function(db_archive) {
+###############################################################################
+
+#' Extracts the relative file-paths for each file within a given CoxpresDB.jp
+#' archive
+#'
+#' @inheritParams   .is_coxpresdb_archive
+#'
+#' @importFrom   gtools        mixedsort
+#' @importFrom   stringr       str_interp
+#'
+#' @return       A vector of file-paths, sorted alphanumerically, each file is
+#' present in the given archive and represents the coexpression data for a
+#' particular gene.
+#'
+#' @export
+#'
+.get_coxpresdb_file_paths <- function(
+                                      db_archive) {
+  stopifnot(.is_coxpresdb_archive(db_archive))
+  files <- system(
+    # nolint start
+    stringr::str_interp("tar --list -jf ${db_archive} | grep -v '/$'"),
+    # nolint end
+    intern = TRUE
+  )
+  gtools::mixedsort(files)
 }
+
+###############################################################################
+
+#' Import the coxpression dataset for a single gene
+#'
+#' @param        gene_id       A single identifier for a gene. This should be
+#' present in the database (an error is thrown if not).
+#'
+#' @inheritParams   .is_coxpresdb_archive
+#'
+#' @return       A single dataframe containing the source -> target mappings,
+#' and both the mutual ranks and correlations between the gene pairs.
+#'
+#' @importFrom   data.table    fread
+#' @importFrom   dplyr         filter_   mutate_
+#' @importFrom   magrittr      %>%   set_colnames
+#' @importFrom   tibble        as_data_frame   data_frame
+#'
+#' @export
+#'
+import_coex_db <- function(
+                           gene_id,
+                           db_archive) {
+  gene_files <- .get_coxpresdb_file_paths(db_archive)
+  stopifnot(
+    length(gene_id) == 1 &&
+      gene_id %in% basename(gene_files)
+  )
+
+  expected_colnames <- c(
+    "source_id", "target_id", "mutual_rank", "correlation"
+  )
+
+  gene_file <- gene_files[basename(gene_files) == gene_id]
+  stopifnot(length(gene_file) == 1)
+
+  coex_db <- data.table::fread(
+    paste("tar --to-stdout -xjf", db_archive, gene_file)
+  ) %>%
+    magrittr::set_colnames(expected_colnames[-1]) %>%
+    tibble::as_data_frame() %>%
+    dplyr::mutate_(
+      source_id = ~ gene_id,
+      target_id = ~ as.character(target_id)
+    ) %>%
+    magrittr::extract(expected_colnames) %>%
+    dplyr::filter_(~ target_id != gene_id)
+
+  coex_db
+}
+
+###############################################################################
+
+#' Imports the gene identifiers that are represented within a given
+#' CoxpresDB.jp archive
+#'
+#' @inheritParams   .is_coxpresdb_archive
+#'
+#' @return       A vector of gene-ids; data for each such gene is present in
+#' the user-supplied archive
+#'
+#' @importFrom   gtools        mixedsort
+#' @export
+#'
+import_coex_db_universe <- function(
+                                    db_archive) {
+  gene_files <- .get_coxpresdb_file_paths(db_archive)
+  basename(gene_files)
+}
+
+###############################################################################
--- tests/testthat/test_coxpresdbr_io.R
+++ tests/testthat/test_coxpresdbr_io.R
@@ -16,6 +16,70 @@ test_data_genes <- as.character(c(
 
 ###############################################################################
 
+test_that(".is_coxpresdb_archive", {
+  expect_equal(
+    object = .is_coxpresdb_archive(test_data_file),
+    expected = TRUE,
+    info = "Checks a single valid coxpresdb archive is a valid archive"
+  )
+
+  expect_equal(
+    object = .is_coxpresdb_archive("test_coxpresdbr_io.R"),
+    expected = FALSE,
+    info = "A valid coxpresdb archive should have a `.tar.bz2 extension`"
+  )
+
+  expect_equal(
+    object = .is_coxpresdb_archive(rep(test_data_file, 2)),
+    expected = FALSE,
+    info = "User should only use a single coxpresdb archive at a time"
+  )
+})
+
+###############################################################################
+
+test_that(".get_coxpresdb_file_paths", {
+  expect_equal(
+    object = .get_coxpresdb_file_paths(test_data_file),
+    expected = sort(file.path("spo_v14_subset", test_data_genes)),
+    info = "File contents of a valid CoxpresDB.jp archive"
+  )
+})
+
+###############################################################################
+
+test_that("import_coex_db_universe: invalid input", {
+  expect_error(
+    object = import_coex_db_universe(db_archive = "NOT A FILE"),
+    info = paste(
+      "Attempt to load a gene-universe from a missing file as",
+      "coxpresdb archive"
+    )
+  )
+
+  expect_error(
+    object = import_coex_db_universe(db_archive = "test_coxpresdbr_io.R"),
+    info = paste(
+      "Attempt to load a gene-universe from an existing file that",
+      "is not a .tar.bz2 coxpresdb archive"
+    )
+  )
+})
+
+test_that("import_coex_db_universe: valid input", {
+  expect_silent(
+    object = import_coex_db_universe(db_archive = test_data_file)
+  )
+
+  expect_equal(
+    object = import_coex_db_universe(db_archive = test_data_file),
+    expected = test_data_genes,
+    info = "Gene-set parsing from a coxpresdb archive"
+  )
+})
+
+###############################################################################
+
 test_that("import_coex_db: invalid input", {
   expect_error(
     object = import_coex_db(gene_id = "2538791", db_archive = "NOT A FILE"),
@@ -25,5 +89,65 @@ test_that("import_coex_db: invalid input", {
   expect_error(
     object = import_coex_db("NOT_A_GENE", db_archive = test_data_file),
     info = "Attempt to import a missing gene from a coxpresdb archive"
+  )
+
+  expect_error(
+    object = import_coex_db(test_data_genes[1:2], db_archive = test_data_file),
+    info = paste(
+      "User should only request the coexpression database for a",
+      "single gene"
+    )
+  )
+})
+
+test_that("import_coex_db: valid input", {
+  message(getwd())
+
+  expect_silent(
+    object = import_coex_db(gene_id = "2538791", db_archive = test_data_file)
+  )
+
+  coex_db_2538791 <- import_coex_db(
+    gene_id = "2538791", db_archive = test_data_file
+  )
+
+  expect_is(
+    object = coex_db_2538791,
+    class = "tbl_df",
+    info = "Coexpression database should be returned as a tibble::tbl_df"
+  )
+
+  expect_equal(
+    object = colnames(coex_db_2538791),
+    expected = c("source_id", "target_id", "mutual_rank", "correlation"),
+    info = paste(
+      "Colnames of a coexpression database should be `source_id`,",
+      "`target_id`, `mutual_rank`, and `correlation`"
+    )
+  )
+
+  expect_equal(
+    object = unique(coex_db_2538791[["source_id"]]),
+    expected = "2538791",
+    info = paste(
+      "A single source-gene should be present in a returned",
+      "coexpression database"
+    )
+  )
+
+  expect_false(
+    object = "2538791" %in% coex_db_2538791[["target_id"]],
+    info = paste(
+      "The source-gene should be absent from the targets in its",
+      "coexpression database"
+    )
+  )
+  expect_equal(
+    object = as.vector(sapply(coex_db_2538791, class)),
+    expected = c("character", "character", "numeric", "numeric"),
+    info = paste(
+      "Coltypes should be character for source/target-id and numeric for",
+      "mutual-rank/correlation"
+    )
   )
 })
