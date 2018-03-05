d109d65c9458dcdeb2cf08b5cef7d973f46284c4
Russell Hyde <russ_hyde@hotmail.com>
2018-03-05 18:45:43 +0000

    functions .filter_coex_partners, .define_default_gene_universe and get_coex_partners; used for import/filter/combine steps to generate coex datasets across multiple source genes and under various filtering schemes

--- R/coxpresdbr_parse.R
+++ R/coxpresdbr_parse.R
@@ -3,12 +3,143 @@
 #
 ###############################################################################
 
+.define_default_gene_universe <- function(coex_df) {
+  stopifnot(
+    all(c("source_id", "target_id") %in% colnames(coex_df))
+  )
+  unique(
+    c(
+      coex_df[["source_id"]],
+      coex_df[["target_id"]]
+    )
+  )
+}
+
+###############################################################################
+
+#' Function to filter the contents of a coxpresdb.jp gene-coexpression dataset
+#'
+#' @param        coex_df       A dataframe containing coexpression data. As
+#' returned by \code{import_coex_db}.
+#'
+#' @param        gene_universe   The genes in the dataframe should be filtered
+#' to ensure they are all present in this set. Note that both the entries in
+#' \code{source_id} and \code{target_id} are filtered to be in this set.
+#'
+#' @param        n_partners    The maximum number of partners to return for
+#' the source gene(s) in \code{coex_df}. Gene partners will be sorted in
+#' order of mutual-rank and then correlation-coefficient before selecting the
+#' top partners.
+#'
+#' @param        mr_threshold   All gene-pairs in the returned dataset will
+#' have a mutual-rank of at most this value.
+#'
+#' @param        cor_threshold   All gene-pairs in the returned dataset will
+#' have a correlation coefficient of at least this value.
+#'
+#' @importFrom   dplyr         filter_
+#' @importFrom   utils         head
+#'
+.filter_coex_partners <- function(
+                                  coex_df,
+                                  gene_universe,
+                                  n_partners,
+                                  mr_threshold,
+                                  cor_threshold) {
+  # TODO: generalise for >= 2
+  if (length(unique(coex_df[["source_id"]])) > 1) {
+    stop(
+      ".filter_coex_partners has not yet been implemented for >1 source gene"
+    )
+  }
+
+  gene_universe <- if (missing(gene_universe) || is.null(gene_universe)) {
+    .define_default_gene_universe(coex_df)
+  } else {
+    gene_universe
+  }
+
+  n_partners <- if (missing(n_partners) || is.null(n_partners)) {
+    nrow(coex_df)
+  } else {
+    n_partners
+  }
+
+  mr_threshold <- if (missing(mr_threshold) || is.null(mr_threshold)) {
+    max(coex_df[["mutual_rank"]])
+  } else {
+    mr_threshold
+  }
+
+  cor_threshold <- if (missing(cor_threshold) || is.null(cor_threshold)) {
+    -1
+  } else {
+    cor_threshold
+  }
+
+  coex_df %>%
+    dplyr::filter_(
+      ~ source_id %in% gene_universe &
+        target_id %in% gene_universe &
+        mutual_rank <= mr_threshold &
+        correlation >= cor_threshold
+    ) %>%
+    dplyr::arrange_(
+      ~ mutual_rank + dplyr::desc(correlation)
+    ) %>%
+    head(n = n_partners)
+}
+
+###############################################################################
+
+#' Returns the coexpression partners of a given set of genes
+#'
+#' Several filters are applied while identifying the coexpression partners of a
+#' given set of genes. Only those gene-pairs with a correlation above- and a
+#' mutual rank below- the user-selected threshold, and for a given input gene
+#' _at most_ \code{n_partners} are returned.
+#'
+#' If the user provides a \code{gene_universe}, the dataframe will only contain
+#' genes that are present in this set (the default universe is the entirety of
+#' the genes present in the coxpresDB archive).
+#'
+#' If the thresholds, number-of-partners or gene_universe are set to NULL,
+#' the corresponding filters are not applied to the dataset.
+#'
+#' @param        gene_ids      A vector of gene identifiers. All of these
+#' should be annotated within the coxpresdb dataset.
+#'
+#' @include      coxpresdbr_io.R
+#'
+#' @inheritParams   .is_coxpresdb_archive
+#' @inheritParams   .filter_coex_partners
+#'
+#' @importFrom   dplyr         bind_rows
+#' @importFrom   purrr         map
+#'
+#' @export
+#'
 get_coex_partners <- function(
                               gene_ids,
                               db_archive,
                               gene_universe = NULL,
                               n_partners = 100,
-                              cor_threshold = -1,
-                              mr_threshold = 0) {
-  NULL
+                              mr_threshold = NULL,
+                              cor_threshold = NULL) {
+  .import_fn <- function(x) {
+    import_coex_db(x, db_archive = db_archive)
   }
+
+  .filter_fn <- function(x) {
+    .filter_coex_partners(
+      x,
+      gene_universe = gene_universe, n_partners = n_partners,
+      mr_threshold = mr_threshold, cor_threshold = cor_threshold
+    )
+  }
+
+  gene_ids %>%
+    purrr::map(.import_fn) %>%
+    purrr::map(.filter_fn) %>%
+    dplyr::bind_rows()
+}
--- tests/testthat/test_coxpresdbr_parse.R
+++ tests/testthat/test_coxpresdbr_parse.R
@@ -6,3 +6,107 @@
 context("Tests for dataset parsing in `coxpresdbr` package")
 
 ###############################################################################
+
+test_that(".filter_coex_partners", {
+  # TODO: compound tests
+
+  df1 <- tibble::data_frame(
+    source_id = rep("A", 5),
+    target_id = LETTERS[2:6],
+    mutual_rank = 1:5,
+    correlation = 1 - 0.1 ^ seq(5, 1)
+  )
+
+  expect_equal(
+    object = .filter_coex_partners(df1),
+    expected = df1,
+    info = "dataset should be unchanged if no filters are applied"
+  )
+
+  expect_equal(
+    object = .filter_coex_partners(df1, gene_universe = c("A", "B", "D")),
+    expected = df1[c(1, 3), ],
+    info = "gene_universe imposes a filter over both source_id and target_id"
+  )
+
+  expect_equal(
+    object = .filter_coex_partners(df1, gene_universe = c("B", "D")),
+    expected = df1[integer(0), ],
+    info = paste(
+      "if none of the source_ids are in the gene_universe, an empty frame",
+      "results"
+    )
+  )
+
+  expect_equal(
+    object = .filter_coex_partners(df1[sample(1:nrow(df1)), ], n_partners = 2),
+    expected = df1[1:2, ],
+    info = "`n_partners` imposes a filter on the number of returned entries"
+  )
+
+  expect_equal(
+    object = .filter_coex_partners(
+      df1[sample(1:nrow(df1)), ], mr_threshold = 4
+    ),
+    expected = df1[1:4, ],
+    info = "`mr_threshold` imposes a filter on the mutual_rank value"
+  )
+
+  expect_equal(
+    object = .filter_coex_partners(
+      df1[sample(1:nrow(df1)), ], cor_threshold = 0.999
+    ),
+    expected = df1[1:3, ],
+    info = "`cor_threshold` imposes a filter on the correlation value"
+  )
+})
+
+###############################################################################
+
+test_that("get_coex_partners", {
+  # TODO: tests for multiple genes and filtering
+
+  test_data_file <- "spo_v14_subset.tar.bz2"
+  test_data_genes <- as.character(c(
+    2538791, 2539499, 2540594, 2541560, 2541907,
+    2542210, 2542294, 2543492, 3361219, 3361512
+  ))
+
+  expect_equal(
+    object = get_coex_partners(
+      gene_ids = "2538791", db_archive = test_data_file
+    ),
+    expected = import_coex_db(
+      gene_id = "2538791", db_archive = test_data_file
+    ),
+    info = paste(
+      "for a single gene and no filters, get_coex_partners should match",
+      "import_coex_db"
+    )
+  )
+
+  expect_equal(
+    object = get_coex_partners(
+      gene_ids = "2538791", db_archive = test_data_file, n_partners = 3
+    ),
+    expected = import_coex_db(
+      gene_id = "2538791", db_archive = test_data_file
+    )[1:3, ],
+    info = paste(
+      "a single gene with a `n_partners` filter"
+    )
+  )
+
+  expect_equal(
+    object = get_coex_partners(
+      gene_ids = c("2538791", "2539499"), db_archive = test_data_file
+      ),
+    expected = bind_rows(
+      import_coex_db("2538791", test_data_file),
+      import_coex_db("2539499", test_data_file)
+      ),
+    info = paste(
+      "a pair of source genes, without any filters"
+      )
+    )
+})
