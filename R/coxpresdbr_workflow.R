06e46d1ee33bae7d9d5933dfc1959f6eae990702
Russell Hyde <russ_hyde@hotmail.com>
2018-03-14 12:30:05 +0000

    adds/checks `gene_universe` details within `run_coex_partner_workflow`.
    Gene-universe argument is either NULL or a subset of both gene_statistics$gene_id and get_gene_ids(importer). If NULL, gene-universe is taken to be the intersection of the latter two sets.

--- R/coxpresdbr_workflow.R
+++ R/coxpresdbr_workflow.R
@@ -4,15 +4,46 @@
 
 ###############################################################################
 
-# TODO: `run_coex_partner_workflow`
+#' Extract partners of a set of gene-ids, generate summary statistics over
+#' those neighbours and then cluster the initial gene-ids
+#'
+#' @param        gene_ids     A set of gene identifiers. Should be a subset of
+#' the genes in \code{gene_statistics} and (if defined) \code{gene_universe}.
+#'
+#' @param        gene_statistics   A table containing the two-tailed p-values,
+#' direction-of-change and gene-ids for all genes that are to be considered in
+#' the analysis (any genes that are not in the `gene_universe` are
+#' disregarded).
+#'
+#' @param        importer      A CoxpresDbImporter object.
+#'
+#' @param        gene_universe   A set of gene identifiers or NULL. If defined,
+#' each of the gene-identifiers should have data present in a row of
+#' \code{gene_statistics}. If undefined, the universe is taken to be the
+#' intersection of all genes in \code{gene_statistics} and all genes accessible
+#' through the CoxpresDB dataset in \code{importer}. Once defined, all genes in
+#' \code{gene_ids} should be present in \code{gene_universe}.
+#'
+#' @param        n_partners    The maximum number of partners to pull out for
+#' a given source gene from the CoxpresDb database.
+#'
+#' @param        ...           Other arguments passed to
+#' \code{get_coex_partners}.
+#'
+#' @importFrom   dplyr         filter_
+#'
+#' @export
+#'
 run_coex_partner_workflow <- function(
                                       gene_ids,
                                       gene_statistics,
                                       importer,
-                                      gene_universe = NULL) {
+                                      gene_universe = NULL,
+                                      n_partners = 100, ...) {
   if (missing(gene_ids) || is.null(gene_ids)) {
-    stop("gene_ids should be deined in run_coex_partner_workflow")
+    stop("`gene_ids` should be defined in `run_coex_partner_workflow`")
   }
+
   if (missing(gene_statistics) ||
     is.null(gene_statistics) ||
     !.is_gene_statistics_df(gene_statistics)
@@ -22,9 +53,37 @@ run_coex_partner_workflow <- function(
       "in `run_coex_partner_workflow`"
     ))
   }
+
   if (missing(importer) || !(is(importer, "CoxpresDbImporter"))) {
-    stop("importer should be defined in run_coex_partner_workflow")
+    stop("`importer` should be defined in `run_coex_partner_workflow`")
   }
+
+  if (is.null(gene_universe)) {
+    gene_universe <- intersect(
+      gene_statistics$gene_id, get_gene_ids(importer)
+    )
+  }
+
+  if (!all(gene_universe %in% gene_statistics[["gene_id"]])) {
+    stop(paste(
+      "If `gene_universe` is provided, all it's elements should be",
+      "present in `gene_statistics$gene_id`"
+    ))
+  }
+
+  if (!all(gene_ids %in% gene_universe)) {
+    stop(paste(
+      "If `gene_universe` is provided, ensure every one of `gene_ids` is",
+      "present inside `gene_universe`"
+    ))
+  }
+
+  new(
+    "CoxpresDbPartners",
+    gene_statistics = dplyr::filter_(
+      gene_statistics, ~ gene_id %in% gene_universe
+    )
+  )
 }
 
 ###############################################################################
--- tests/testthat/test_coxpresdbr_workflows.R
+++ tests/testthat/test_coxpresdbr_workflows.R
@@ -82,8 +82,35 @@ test_that("run_coex_partner_workflow: invalid input", {
     ),
     info = "gene_statistics must pass .is_gene_statistics_df"
   )
-  # TODO - pass this test:
   # - fail on gene_statistics that is a subset of gene_universe
+  expect_error(
+    object = run_coex_partner_workflow(
+      gene_ids = test_data_genes,
+      gene_statistics = test_gene_statistics[-1, ],
+      importer = test_importer,
+      gene_universe = test_data_genes
+    ),
+    info = paste(
+      "if gene_universe is provided, all it's elements should be present in",
+      "gene_statistics$gene_id"
+    )
+  )
+
+  # - fail on importer for which it's gene-ids are a subset of gene_universe
+  expect_error(
+    object = run_coex_partner_workflow(
+      gene_ids = test_data_genes[1:2],
+      gene_statistics = test_gene_statistics,
+      importer = test_importer,
+      gene_universe = c(test_data_genes, "not_a_gene")
+    ),
+    info = paste(
+      "if gene_universe is provided, all it's elements should have data",
+      "inside the coexpression database that is being mined"
+    )
+  )
+
+  # - fail on gene_universe that is a subset of gene_ids
   expect_error(
     object = run_coex_partner_workflow(
       gene_ids = test_data_genes,
@@ -91,18 +118,42 @@ test_that("run_coex_partner_workflow: invalid input", {
       importer = test_importer,
       gene_universe = test_data_genes[-1]
     ),
-    info = paste(
-      "if gene_universe is provided, all genes in gene_statistics should be",
-      "present in gene_universe"
-    )
+    info = "if gene_universe is provided, it should contain all of `gene_id`s"
   )
-  # - fail on gene_universe that is a subset of gene_ids
-  #
 })
 
 test_that("run_coex_partner_workflow: valid input", {
 
   # output should be a CoxpresDbPartners object
+
+  # when gene_universe is NULL, it should equal the intersection between
+  # the gene-ids present in CoxpresDB and in `gene_statistics`
+  non_overlap_gene_statistics <- bind_rows(
+    test_gene_statistics[-c(1:2), ],
+    tibble::data_frame(
+      gene_id = "1234567", p_value = 0.4, direction = -1
+    )
+  )
+
+  expect_equal(
+    object = get_gene_universe(
+      run_coex_partner_workflow(
+        gene_ids = test_data_genes[3:6],
+        gene_statistics = non_overlap_gene_statistics,
+        importer = test_importer,
+        gene_universe = NULL
+      )
+    ),
+    expected = intersect(
+      non_overlap_gene_statistics$gene_id,
+      get_gene_ids(test_importer)
+    ),
+    info = paste(
+      "If `gene_universe` is NULL, it should be defined to contain only those",
+      "genes that are in both `gene_statistics` and accessible through",
+      "`importer`"
+    )
+  )
 
   # `gene_stats` entry should be the restriction of `gene_statistics` input to
   # `gene_universe`
