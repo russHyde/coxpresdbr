0d66c59e586b82fab04fa8366637bbdf6e4102d7
Russell Hyde <russ_hyde@hotmail.com>
2019-07-23 17:16:51 +0100

    remove dependence on purrr
    Also some doc/include fixes

--- R/coxpresdbr_parse.R
+++ R/coxpresdbr_parse.R
@@ -17,27 +17,28 @@
 
 ###############################################################################
 
-#' Function to filter the contents of a coxpresdb.jp gene-coexpression dataset
+#' Function to filter the contents of a 'coxpresdb.jp' gene-coexpression
+#' dataset
 #'
 #' @param        coex_df       A dataframe containing coexpression data. As
-#' returned by \code{import_all_coex_partners}.
+#'   returned by \code{import_all_coex_partners}.
 #'
 #' @param        gene_universe   The genes in the dataframe should be filtered
-#' to ensure they are all present in this set. Note that both the entries in
-#' \code{source_id} and \code{target_id} are filtered to be in this set.
+#'   to ensure they are all present in this set. Note that both the entries in
+#'   \code{source_id} and \code{target_id} are filtered to be in this set.
 #'
-#' @param        n_partners    The maximum number of partners to return for
-#' the source gene(s) in \code{coex_df}. Gene partners will be sorted in
-#' order of mutual-rank and then correlation-coefficient before selecting the
-#' top partners.
+#' @param        n_partners    The maximum number of partners to return for the
+#'   source gene(s) in \code{coex_df}. Gene partners will be sorted in order of
+#'   mutual-rank and then correlation-coefficient before selecting the top
+#'   partners.
 #'
 #' @param        mr_threshold   All gene-pairs in the returned dataset will
-#' have a mutual-rank of at most this value.
+#'   have a mutual-rank of at most this value.
 #'
 #' @param        cor_threshold   All gene-pairs in the returned dataset will
-#' have a correlation coefficient of at least this value.
+#'   have a correlation coefficient of at least this value.
 #'
-#' @importFrom   dplyr         filter_
+#' @importFrom   dplyr         arrange_   filter_   desc
 #' @importFrom   utils         head
 #'
 .filter_coex_partners <- function(
@@ -46,10 +47,9 @@
                                   n_partners,
                                   mr_threshold,
                                   cor_threshold) {
-  # TODO: generalise for >= 2
   if (length(unique(coex_df[["source_id"]])) > 1) {
     stop(
-      ".filter_coex_partners has not yet been implemented for >1 source gene"
+      "`.filter_coex_partners` has not yet been implemented for >1 source gene"
     )
   }
 
@@ -103,21 +103,20 @@
 #' genes that are present in this set (the default universe is the entirety of
 #' the genes present in the coxpresDB archive).
 #'
-#' If the thresholds, number-of-partners or gene_universe are set to NULL,
-#' the corresponding filters are not applied to the dataset.
+#' If the thresholds, number-of-partners or gene_universe are set to
+#' \code{NULL}, the corresponding filters are not applied to the dataset.
 #'
 #' @param        gene_ids      A vector of gene identifiers. All of these
-#' should be annotated within the coxpresdb dataset.
+#'   should be annotated within the 'coxpresDb' dataset.
 #'
-#' @param        importer      A CoxpresDbImporter. This allows access to a
-#' coxpresDb archive.
+#' @param        importer      A \code{CoxpresDbImporter}. This allows access
+#'   to a 'coxpresDb' archive.
 #'
 #' @include      coxpresdbr_io.R
 #'
 #' @inheritParams   .filter_coex_partners
 #'
 #' @importFrom   dplyr         bind_rows
-#' @importFrom   purrr         map
 #'
 #' @export
 #'
@@ -140,10 +139,10 @@ get_coex_partners <- function(
     )
   }
 
-  gene_ids %>%
-    purrr::map(.import_fn) %>%
-    purrr::map(.filter_fn) %>%
-    dplyr::bind_rows()
+  imported <- Map(.import_fn, gene_ids)
+  filtered <- Map(.filter_fn, imported)
+
+  dplyr::bind_rows(filtered)
 }
 
 ###############################################################################
