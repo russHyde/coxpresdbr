ea63fecf7ce0c288b4aac05616914b965230235c
Russ Hyde <russHyde@users.noreply.github.com>
2019-07-29 17:07:30 +0100

    Data-frame accessor (#22)
    * add constructor for `CoxpresDbDataframeAccessor`
    
    `CoxpresDbDataframeAccessor` inherits from
    `CoxpresDbAccessor`.
    
    The dataframe in a `CoxpresDbDataframeAccessor` should
    contain `source_id`, `target_id` and `mutual_rank` columns.
    
    * add methods for `CoxpresDbDataframeAccessor`
    
    Generalised `get_gene_ids` and `get_all_coex_partners`

--- tests/testthat/test_coxpresdbr_CoxpresDbArchive.R
+++ tests/testthat/test_coxpresdbr_CoxpresDbArchive.R
@@ -37,6 +37,68 @@ test_that("CoxpresDbArchiveAccessor: class definition", {
     class = "CoxpresDbAccessor",
     info = "new `CoxpresDbArchiveAccessor` is a subclass of `CoxpresDbAccessor`"
   )
+
+  # TODO: object creation using the function `CoxpresDbAccessor`
 })
 
 ###############################################################################
+
+test_that("CoxpresDbDataframeAccessor: class definition", {
+  # Note that CoxpresDbDataframeAccessor 'new' constructor is not exported
+  # so the user does not have access to the following code.
+
+  archive_accessor <- methods::new("CoxpresDbDataframeAccessor")
+
+  expect_is(
+    object = archive_accessor,
+    class = "CoxpresDbDataframeAccessor",
+    info = "new `CoxpresDbDataframeAccessor` is a `CoxpresDbDataframeAccessor`"
+  )
+
+  expect_is(
+    object = archive_accessor,
+    class = "CoxpresDbAccessor",
+    info = paste(
+      "new `CoxpresDbDataframeAccessor` is a subclass of `CoxpresDbAccessor`"
+    )
+  )
+
+  df <- tibble::tibble(
+    source_id = "123",
+    target_id = "456",
+    mutual_rank = 1.357
+  )
+
+  expect_is(
+    object = CoxpresDbAccessor(df),
+    class = "CoxpresDbDataframeAccessor",
+    info = paste(
+      "User can make a `CoxpresDbDataframeAccessor` by passing a `data.frame`",
+      "to the `CoxpresDbAccessor` function"
+    )
+  )
+
+  # If the user provides a dataframe for in-memory access to CoxpresDb, then
+  # the columns `source_id`, `target_id`, `mutual_rank` should all be present
+  expect_error(
+    object = CoxpresDbAccessor(dplyr::rename(df, not_source = source_id)),
+    info = paste(
+      "If user makes a `CoxpresDbAccessor` from a `dataframe`, then",
+      "`source_id` should be present in columns"
+    )
+  )
+  expect_error(
+    object = CoxpresDbAccessor(dplyr::rename(df, not_target = target_id)),
+    info = paste(
+      "If user makes a `CoxpresDbAccessor` from a `dataframe`, then",
+      "`target_id` should be present in columns"
+    )
+  )
+  expect_error(
+    object = CoxpresDbAccessor(dplyr::rename(df, not_rank = mutual_rank)),
+    info = paste(
+      "If user makes a `CoxpresDbAccessor` from a `dataframe`, then",
+      "`mutual_rank` should be present in columns"
+    )
+  )
+})
